463e0ce62072f4d36e2f8b40563b9cb3fd68fb57
Pierre7Agus <redtor_90@outlook.es>
2023-06-25 02:43:23 -0500

    Add styles and more
    The component was perfect, I applied a small animation. Try not to abuse flexbox to center elements, now what I have to do is add functionality to it so that it can be expanded. I have also thought about creating this component using template

---
+++ componente-carrito/app.js
@@ -0,0 +1,11 @@
+/*
+const $contenedor = document.getElementById("minicarrito__contenedor");
+document.addEventListener("click", (e) => {
+	if (e.target.matches(".minicarrito--contenedor *")) {
+		setInterval(() => {
+			$contenedor.classList.toggle("ape-jello");
+			$contenedor.classList.toggle("animated");
+		}, 5000);
+	}
+});
+*/
