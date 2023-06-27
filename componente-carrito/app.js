a0fb4663536f168a21780bc32e40a56ca89dc1dd
Pierre7Agus <redtor_90@outlook.es>
2023-06-27 12:51:03 -0500

    Modify UI
    The UI was modified so that by default the small cart is shown first, then a bit of javascript was added to modify some styles in order to show or hide the carts. In this case, the styles were modified with javascript, so the styles are applied online, another way would have been to do it by applying classes

--- componente-carrito/app.js
+++ componente-carrito/app.js
@@ -18,6 +18,21 @@ document.querySelector(".carro-flecha").addEventListener("click", (e) => {
 		$carroContenedor.style.left = "-500px";
 		setTimeout(() => {
 			$carroContenedor.style.display = "none";
-		}, 1000);
+		}, 500);
+	}
+});
+
+const $contenedorMiniCarrito = document.getElementById(
+	"minicarrito__contenedor"
+);
+$contenedorMiniCarrito.addEventListener("click", (e) => {
+	if (
+		e.target.matches(".minicarrito--contenedor") ||
+		e.target.matches(".minicarrito--contenedor *")
+	) {
+		$carroContenedor.style.display = "block";
+		setTimeout(() => {
+			$carroContenedor.style.left = "0px";
+		}, 10);
 	}
 });
